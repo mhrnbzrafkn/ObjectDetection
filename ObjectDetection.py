39c4a5569a7864d6fe4e6db37d9bbf02626f2b51
m.bazrafkn <mhrn.bzrafkn.dev@gmail.com>
2023-03-13 14:46:49 -0700

    initial object detection completed

---
+++ ObjectDetection.py
@@ -0,0 +1,38 @@
+import cv2 
+import numpy as np
+import matplotlib.pyplot as plt
+
+cap = cv2.VideoCapture('video1.avi')
+#cap = cv2.VideoCapture(0);
+
+
+ret, fram1 = cap.read()
+ret, fram2 = cap.read()
+
+
+height = fram1.shape[0]
+width = fram1.shape[1]
+sc = width/height
+w = 500
+
+while cap.isOpened():
+    fram1 = cv2.resize(fram1, (int(w*sc), w))
+    fram2 = cv2.resize(fram2, (int(w*sc), w))
+    
+    diff = cv2.absdiff(fram1, fram2)
+    gray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    _, thresh = cv2.threshold(blur,20,255,cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh,None,iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(fram1, contours,-1,(0,255,0),2)
+
+    cv2.imshow("feed", fram1)
+    fram1 = fram2
+    ret, fram2 = cap.read()
+
+    if cv2.waitKey(40) == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
